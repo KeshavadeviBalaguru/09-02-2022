dbeab51b87413593aee42b8916fa0299f5fd2785
ELCOT <ELCOT@Lenovo>
2022-03-25 18:50:33 +0530

    first commit

---
+++ 09-02-2022/src/main/java/com/Mavenproject/App2.java
@@ -0,0 +1,12 @@
+package com.Mavenproject;
+
+public class App2 {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=25;
+		System.out.println(a+b);
+
+	}
+
+}
---
+++ 09-02-2022/src/main/java/com/Mavenproject/App3.java
@@ -0,0 +1,12 @@
+package com.Mavenproject;
+
+public class App3 {
+	public static void main(String[] args){
+	int a =10;
+	int b =6;
+	
+	System.out.println(a-b);
+	}
+
+
+}
